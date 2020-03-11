c4bb9b625597716a8e77fbed2169ddfc34cbaba8
Guille <guillermo.varelli@xpo.com>
2020-03-11 10:52:57 -0300

    encapsulamiento e inmutabilidad

--- src/main/java/com/example/entrenamiento/_2_PrincipiosyPatrones/_2_2_Predicate/Main.java
+++ src/main/java/com/example/entrenamiento/_2_PrincipiosyPatrones/_2_2_Predicate/Main.java
@@ -2,6 +2,7 @@ package com.example.entrenamiento._2_PrincipiosyPatrones._2_2_Predicate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 @FunctionalInterface
@@ -39,6 +40,10 @@ class Animal {
     }
 }
 
+@FunctionalInterface
+interface ClasificadorDeAnimales {
+    boolean chequear(Animal animal);
+}
 
 public class Main {
     static void clasificar(Animal animal, Predicate<Animal> clasificadorDeAnimales) {
@@ -55,17 +60,18 @@ public class Main {
 
         clasificar(new Animal("PEZ", true, false), //ANIMAL
                 animal -> animal.puedeNadar);//interfaz funcional
-        Predicate<Integer> filterlower = x -> x>2;
+        Predicate<Integer> filtrarMenores = x-> x>2;
         List<Integer> ints = Arrays.asList(1,2,3,4,5,6,7,8);
-        ints.stream().filter(filterlower).filter(new Predicate<Integer>() {
+        ints.stream().filter(filtrarMenores).filter(new Predicate<Integer>() {
             @Override
             public boolean test(Integer integer) {
-                return integer < 7;
+                return integer < 6;
 
             }
-        }).forEach(x-> System.out.println(x));
+        }).forEach(x -> System.out.println(x));
+        BiPredicate<String , String> validateEqulas = (a , b) -> a.equals(b);
+        
+        System.out.println(validateEqulas);
     }
-
-
 }
 
---
+++ src/main/java/com/example/entrenamiento/_2_PrincipiosyPatrones/_2_3_EncapsulamientoInmutabilidad/Main.java
@@ -0,0 +1,50 @@
+package com.example.entrenamiento._2_PrincipiosyPatrones._2_3_EncapsulamientoInmutabilidad;
+
+
+class Casa {
+    int precio;
+    static   String barrio;
+
+    public Casa(int precio, String barrio) {
+        this.precio = precio;
+        this.barrio = barrio;
+    }
+
+    public String getBarrio() {
+        return barrio;
+    }
+
+    public void setBarrio(String barrio) {
+        this.barrio = barrio;
+    }
+}
+
+final class Persona{
+    private String nombre ;
+    private String dni;
+
+    public Persona(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Casa casa1 = new Casa(10000, "centro");
+        Casa casa2 = new Casa(10000, "semi priviado");
+        casa1.setBarrio("privado");
+
+        System.out.println("La casa 1 esta en " + casa1.getBarrio() + " la casa 2 se encuentra en " + casa2.getBarrio());
+
+        String saludo = "Hola";
+        saludo = saludo.toUpperCase();
+        StringBuilder saludar= new StringBuilder("Hello");
+        saludar.append(" word");
+        System.out.println(saludo);
+        System.out.println(saludar);
+    }
+}
